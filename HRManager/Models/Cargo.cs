8b9144c4272c0843dc2242e7a5fc32cdf72b8568
IkerMiguel <ikmiguel03@gmail.com>
2024-11-17 20:29:48 -0500

    Add Model Cargo

---
+++ HRManager/Models/Cargo.cs
@@ -0,0 +1,9 @@
+﻿namespace HRManager.Models
+{
+	public class Cargo
+	{
+		public int IdCargo { get; set; }
+		public string TituloCargo { get; set; }
+		public string Descripcion { get; set; }
+	}
+}
