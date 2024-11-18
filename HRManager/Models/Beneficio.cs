b2f009a5b535b0ece5faa81b9eac78a68f0aacc8
IkerMiguel <ikmiguel03@gmail.com>
2024-11-17 20:33:03 -0500

    Add Model Beneficio

---
+++ HRManager/Models/Beneficio.cs
@@ -0,0 +1,10 @@
+﻿namespace HRManager.Models
+{
+	public class Beneficio
+	{
+		public int IdBeneficio { get; set; }
+		public string Descripcion { get; set; }
+		public decimal CostoEmpresa { get; set; }
+		public decimal CostoEmpleado { get; set; }
+	}
+}
