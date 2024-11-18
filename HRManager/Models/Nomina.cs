8c85be4613442d57564d1f78bcba870096a7b912
IkerMiguel <ikmiguel03@gmail.com>
2024-11-17 20:31:57 -0500

    Add Model Nomina

---
+++ HRManager/Models/Nomina.cs
@@ -0,0 +1,12 @@
+﻿namespace HRManager.Models
+{
+	public class Nomina
+	{
+		public int IdNomina { get; set; }
+		public int IdEmpleado { get; set; }
+		public Empleados? Empleados { get; set; }
+		public DateTime PeriodoInicio { get; set; }
+		public DateTime PeriodoFin { get; set; }
+		public decimal TotalPagado { get; set; }
+	}
+}
