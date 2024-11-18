ec8822af7087039defda959881a6d695d19b0cbf
IkerMiguel <ikmiguel03@gmail.com>
2024-11-17 20:20:02 -0500

    Add Model Empleado

---
+++ HRManager/Models/Empleado.cs
@@ -0,0 +1,17 @@
+﻿namespace HRManager.Models
+{
+	public class Empleados
+	{
+		public int IdEmpleado { get; set; }
+		public string Nombre { get; set; }
+		public string Apellido { get; set; }
+		public string Email { get; set; }
+		public string Telefono { get; set; }
+		public DateTime FechaContratacion { get; set; }
+		public int IdDepartamento { get; set; }
+		public Departamento? Departamento { get; set; }
+		public decimal Salario { get; set; }
+		public int IdCargo { get; set; }
+		public Cargo? Cargo { get; set; }
+	}
+}
