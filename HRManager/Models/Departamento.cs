f69cedc6277fd7e7f0dfedc9da901bcb9940d72d
IkerMiguel <ikmiguel03@gmail.com>
2024-11-17 20:27:09 -0500

    Add Model Departamento

---
+++ HRManager/Models/Departamento.cs
@@ -0,0 +1,9 @@
+﻿namespace HRManager.Models
+{
+	public class Departamento
+	{
+		public int IdDepartamento { get; set; }
+		public string NombreDepartamento { get; set; }
+		public string Ubicacion { get; set; }
+	}
+}
