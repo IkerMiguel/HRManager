851bb0b48a94f52e4355145104ba8ff8172f6a9d
Emilee09 <emileeelenap@gmail.com>
2024-11-19 13:20:24 -0500

    se creo el edit de beneficio

---
+++ HRManager/Pages/Beneficios/Edit.cshtml.cs
@@ -0,0 +1,71 @@
+using HRManager.Data;
+using HRManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRManager.Pages.Beneficios
+{
+    public class EditModel : PageModel
+    {
+        private readonly ManagerContext _context;
+
+        public EditModel(ManagerContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Beneficio Beneficio { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Beneficios == null)
+            {
+                return NotFound();
+            }
+
+            var beneficio = await _context.Beneficios.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (beneficio == null)
+            {
+                return NotFound();
+            }
+            Beneficio = beneficio;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Beneficio).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BeneficioExists(Beneficio.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool BeneficioExists(int id)
+        {
+            return (_context.Beneficios?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
